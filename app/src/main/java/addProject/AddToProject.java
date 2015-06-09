63e2348674c70755d6d2e2d5fd576722a67e7e4c
MoLt1eS <tjrs02@gmail.com>
2015-06-09 18:32:10 +0100

    correcao commit anterior

--- app/src/main/java/addProject/AddToProject.java
+++ app/src/main/java/addProject/AddToProject.java
@@ -222,13 +222,12 @@ public class AddToProject extends AppCompatActivity implements TextWatcher {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inf later.inflate(R.menu.menu_add_to_project, menu);
+        inflater.inflate(R.menu.menu_add_to_project, menu);
         menu.findItem(R.id.nextBut).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         return true;
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
-        
         // Handle presses on the action bar items
         switch (item.getItemId()) {
             case R.id.nextBut:
